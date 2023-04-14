265e7e0801c9bee1c6451f9c1699db892c42e783
AdamWardVGP <AdamWardVGP@gmail.com>
2023-04-14 14:17:12 -0700

    Created Pause Menu

--- Source/ActionRoguelike/Private/ActPlayerController.cpp
+++ Source/ActionRoguelike/Private/ActPlayerController.cpp
@@ -3,6 +3,38 @@
 
 #include "ActPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
+
+void AActPlayerController::TogglePauseMenu()
+{
+	if(PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+		return;
+	} 
+
+
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+	if(PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void AActPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("PauseMenu", IE_Pressed, this, &AActPlayerController::TogglePauseMenu);
+}
+
 void AActPlayerController::SetPawn(APawn* InPawn)
 {
 	Super::SetPawn(InPawn);
--- Source/ActionRoguelike/Public/ActPlayerController.h
+++ Source/ActionRoguelike/Public/ActPlayerController.h
@@ -19,6 +19,17 @@ class ACTIONROGUELIKE_API AActPlayerController : public APlayerController
 
 protected:
 
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePauseMenu();
+
+	void SetupInputComponent() override;
+
 	UPROPERTY(BlueprintAssignable)
 	FOnPawnChanged OnPawnChanged;
 
